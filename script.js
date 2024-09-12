1db190c7b6517075ab1d9afc9a72fdbd06c56bb2
Bheemesh Gadige <gbheemesh888@gmail.com>
2024-09-12 20:45:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 function fibonacci(num) {
 // your code here
+	if(num===1)
+	{
+		return 0;
+	}
+	else if(num===2)
+	{
+		return 1;
+	}
+	else{
+		let var1 = 0;
+		let var2 = 1;
+		let fibonacci=0;
+		for(let i=3;i<=num;i++)
+			{
+				fibonacci = var2+i;
+				var2 = fibonacci;
+			}
+		return fibonacci;
+	}
+	
 }
 
 module.exports = fibonacci;
